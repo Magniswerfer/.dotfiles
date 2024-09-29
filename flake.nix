12e1fc4f9090bd9d7c2535b414b269c6da2f322b
magniswerfer <magnushoholt@pm.me>
2024-09-29 11:52:58 +0200

    My first flakes and home-manager setup

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+    description = "My first flake, basically just my configuration.nix for now lol";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+        home-manager.url = "github:nix-community/home-manager/release-24.05";
+        home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    outputs = {self, nixpkgs, home-manager, ...}: 
+    
+        let 
+            system = "x86_64-linux";
+            lib  = nixpkgs.lib;
+            pkgs = nixpkgs.legacyPackages.${system};
+        
+        in {
+            nixosConfigurations = {
+                t480 = lib.nixosSystem {
+                    inherit system;
+                    modules = [ ./configuration.nix ];
+
+                };
+            };
+            homeConfigurations = {
+                magnushk = home-manager.lib.homeManagerConfiguration {
+                    inherit pkgs;
+                    modules = [ ./home.nix]
+                };
+            };
+        };
+}
